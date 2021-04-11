d4b1f923d864a861df1a721ca484d265bc3d1014
Mikhail Blazhko <michelin8585@gmail.com>
2021-04-11 17:47:46 +0300

    solve 11-15 tasks

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,29 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
-
-  // let storage = {};
-  // let result = '';
-  // const strArr = str.split('');
-  // strArr.forEach((el, idx) => {
-  //   if (el === strArr[idx - 1]) {
-  //     if (!storage[el]) {
-  //       storage[el] = 2;
-  //     } else {
-  //       storage += 1;
-  //     }
-  //   }
-  // });
-  // strArr.forEach((el) => {
-  //   if (storage[el] && result.charAt(result.length - 1) !== el) {
-  //     result += `${storage[el]}${el}`;
-  //   } else if (!storage[el]) {
-  //     result += el;
-  //   }
-  // });
-  // return result;
+function encodeLine(str) {
+  const storage = {};
+  let result = '';
+  const strArr = str.split('');
+  strArr.forEach((el, idx) => {
+    if (el === strArr[idx - 1]) {
+      if (!storage[el]) {
+        storage[el] = 2;
+      } else {
+        storage[el] += 1;
+      }
+    }
+  });
+  strArr.forEach((el) => {
+    if (storage[el] && result.charAt(result.length - 1) !== el) {
+      result += `${storage[el]}${el}`;
+    } else if (!storage[el]) {
+      result += el;
+    }
+  });
+  return result;
 }
 
 module.exports = encodeLine;
--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,31 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const result = {};
+  const regsArr = new Set();
+  let splitedDomains = [];
+  splitedDomains = domains.map((el) => el.split('.'));
+  /* create regexps */
+  splitedDomains.forEach((domain) => {
+    regsArr.add(`${domain[domain.length - 1]}`);
+    if (domain.length === 2) {
+      regsArr.add(`${domain[0]}.${domain[1]}`);
+    } else {
+      regsArr.add(`${domain[0]}.${domain[1]}.${domain[2]}`);
+    }
+  });
+  regsArr.forEach((regexp) => {
+    let counter = 0;
+    domains.forEach((domain) => {
+      if (new RegExp(regexp).test(domain)) {
+        counter++;
+      }
+    });
+    const prepairKey = `.${regexp.split('.').reverse().join('.')}`;
+    result[prepairKey] = counter;
+  });
+  return result;
 }
 
 module.exports = getDNSStats;
